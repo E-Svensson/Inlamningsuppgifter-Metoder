da1d3d6b75d6cc40c44e61cfb8cbb8d548385b6d
Bao Lan <89245290+BaoLanNguyen@users.noreply.github.com>
2023-02-17 09:44:50 +0100

    erghertger

---
+++ Uppgift 6.10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace nbjgsbrk
+{
+    class Program
+    {
+        static int AntalTalIText(string para)
+        {
+            string[] strings = para.Split();
+            int antal = 0;
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                bool ÄrDetEttTal = double.TryParse(strings[i], out double value);
+                if (ÄrDetEttTal)
+                    antal++;
+                ÄrDetEttTal = false;
+            }
+
+            return antal;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\nSkriv in ett antal ord eller tal med mellanslag som mellanrum");
+
+            Console.Write("\nSkriv här: "); string input = Console.ReadLine();
+
+            int antal = AntalTalIText(input);
+
+            Console.WriteLine($"\nDet du skrev innehöll {antal} tal");
+        }
+    }
+}
